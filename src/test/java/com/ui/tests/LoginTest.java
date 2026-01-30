7d9a3e10881c23954b32c116fa666c176efcb8fc
Madhav <maddyp238@gmail.com>
2026-01-30 17:43:59 +0530

    updated the data providers path

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -9,7 +9,7 @@ import com.ui.pojo.User;
 
 @Listeners({ com.ui.listeners.MyTestListener.class })
 public class LoginTest extends BaseTest {
-	
+
 	@Test(description = "Verify if the valid user is able to login into the application", groups = {
 			"smoke" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestDataProvider")
 	public void loginTest(User user) {
@@ -17,25 +17,20 @@ public class LoginTest extends BaseTest {
 				"Madhav Pathak");
 	}
 
-	/*
-	 * @Test(description =
-	 * "Verify if the valid user is able to login into the application", groups = {
-	 * "smoke" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class,
-	 * dataProvider = "LoginTestCSVDataProvider") public void loginCSVTest(User
-	 * user) {
-	 * 
-	 * assertEquals(homePage.gotoLoginPage().doLoginWith(user.getEmailAddress(),
-	 * user.getPassword()).getUserName(), "Madhav Pathak"); }
-	 * 
-	 * @Test(description =
-	 * "Verify if the valid user is able to login into the application", groups = {
-	 * "smoke" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class,
-	 * dataProvider = "LoginTestExcelDataProvider", retryAnalyzer =
-	 * com.ui.listeners.MyRetryAnalyzer.class) public void loginExcelTest(User user)
-	 * {
-	 * 
-	 * assertEquals(homePage.gotoLoginPage().doLoginWith(user.getEmailAddress(),
-	 * user.getPassword()).getUserName(), "Madhav Pathak"); }
-	 */
+	@Test(description = "Verify if the valid user is able to login into the application", groups = {
+			"smoke" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVDataProvider")
+	public void loginCSVTest(User user) {
+
+		assertEquals(homePage.gotoLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Madhav Pathak");
+	}
+
+	@Test(description = "Verify if the valid user is able to login into the application", groups = {
+			"smoke" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider", retryAnalyzer = com.ui.listeners.MyRetryAnalyzer.class)
+	public void loginExcelTest(User user) {
+
+		assertEquals(homePage.gotoLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Madhav Pathak");
+	}
 
 }
